41a22fc39aaa1444b1d88c931bfc5eb7fd09ed9b
disha-baghel <bagheldisha708@gmail.com>
2023-10-13 23:26:09 +0530

    optimised code

--- Alien Invasion/ship.py
+++ Alien Invasion/ship.py
@@ -5,15 +5,24 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
-        self.screem_rect = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('Images/spaceship.webp')
         self.rect = self.image.get_rect()
 
-        # Start each ship at its current location
-        self.screen.blit(self.image, self.rect)
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #Movement flag 
+        self.moving_right = False
+
+    def update(self):
+        '''Update the ship's position based on the movement flag'''
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         '''Draw the ship at its currnet location'''
         self.screen.blit(self.image, self.rect)
+        
