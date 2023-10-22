f9f5ce33af2305843496af02e22ea6cce839b273
disha-baghel <bagheldisha708@gmail.com>
2023-10-22 15:53:22 +0530

    created a bullet class for shooting bullets of the ship

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -13,15 +13,22 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))   #surface
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))   #surface
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Imvasion")
         self.ship = Ship(self)
 
         self.bg_color = (17, 24, 39)
+
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
 
     def run_game(self):
         
